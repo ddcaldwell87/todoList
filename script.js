e4de7a65f6ecb429393b06bf38e65989f7126b9c
David Caldwell <ddcaldwell87@gmail.com>
2017-06-18 17:22:31 -0400

    added toggleAll function

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var todoList = {
 		this.todos.splice(position, 1);
 		this.displayTodos();
 	},
-	// toggles the completed status of a todo item
+	// toggles the completed status of a single todo item
 	toggleCompleted: function(position) {
 		if (this.todos[position].completed === false) {
 			this.todos[position].completed = true;
@@ -34,6 +34,29 @@ var todoList = {
 	},
 	// toggles all todos items completed status to true or false
 	toggleAll: function() {
+		// get total number of todo items
+		var totalTodos = this.todos.length;
+		
+		// how many todo items are complete
+		var completedTodos = 0;
 
+		// find out how many todo items are completed and add them to completedTodos
+		for (var i = 0; i < totalTodos; i++) {
+			if (this.todos[i].completed === true) {
+				completedTodos++;
+			}
+		}
+
+		// if all todo items are completed, make them uncompleted
+		if (completedTodos === totalTodos) {
+			for (var i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = false;
+			}
+		// else, make them all completed
+		} else {
+			for (var i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = true;
+			}
+		}
 	}
 };
